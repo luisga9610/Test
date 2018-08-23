a0613e1edc88a6501882d67b9182baa8952d25b1
Alumno <Alumno@ws204-04>
2018-08-22 18:20:19 -0700

    otra actualizacion

--- src/pack1/Operaciones.java
+++ src/pack1/Operaciones.java
@@ -2,4 +2,16 @@ package pack1;
 
 public enum Operaciones {
 
+	SUMA("+"),RESTA("-"),MULTIPLICACION("*"),DIVISION("/");
+	
+	private String valor;
+	
+	Operaciones(String valor){
+		this.valor=valor;
+	}
+	
+	public String getvalor(){
+		return valor;
+	}
+	
 }
--- src/pack1/Test.java
+++ src/pack1/Test.java
@@ -1,6 +1,13 @@
 package pack1;
 
 public class Test {
+	
+	public static void main(String[] args){
 
 	System.out.println(Operaciones.MULTIPLICACION);
+	System.out.println(Operaciones.SUMA.getvalor());
+	System.out.println(Operaciones.RESTA.name());
+	}
+	
+	
 }
